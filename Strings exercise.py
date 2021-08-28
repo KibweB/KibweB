0306c6a5989aeb9d66d0371166226bb46403a606
KibweB <47197142+KibweB@users.noreply.github.com>
2021-08-28 17:08:52 -0400

    Strings & numbers practice

---
+++ Strings exercise.py
@@ -0,0 +1,7 @@
+phrase = "Kahn Academy"
+print(phrase+ " is helpful")
+print(phrase.lower())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase.index("Aca"))
+print(phrase.replace("Kahn", "Zebra"))
---
+++ Working numbers.py
@@ -0,0 +1,9 @@
+from math import*
+my_num=-7
+print(sqrt(36))
+print(str(my_num)+ " is my favorite number")
+print(abs(my_num))
+print(floor (4.6))
+print(ceil(4.6))
+print(2*(3+18))
+print(pow(4,6))
