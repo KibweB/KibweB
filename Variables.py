4320c172eec747568ae0497bc764312c8b4bf216
KibweB <47197142+KibweB@users.noreply.github.com>
2021-08-28 15:40:43 -0400

    simple variables script

---
+++ Variables.py
@@ -0,0 +1,7 @@
+character_name="Waltz Mahoney"
+character_age="38"
+print("There once was a man named " +character_name+ ",")
+print("he was "+character_age+ " years old.")
+character_name="Grayson"
+print("He really disliked the name " +character_name+ ",")
+print("but enjoyed being " +character_age+ "." )
