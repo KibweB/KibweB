28ad91705ba6320c9367e875ea82d6cd8c009814
KibweB <47197142+KibweB@users.noreply.github.com>
2021-08-31 13:36:14 -0400

    Folder organizing

---
+++ Simple code/inputprogram.py
@@ -0,0 +1,4 @@
+name=input("Enter your whole name: ")
+age=input("And how old are you? ")
+print("Your full name is",name)
+print("How's it hanging "+name+"! You're "+age+" years old, cool story!")
--- inputprogram.py
+++
@@ -1,4 +0,0 @@
-name=input("Enter your whole name: ")
-age=input("And how old are you? ")
-print("Your full name is",name)
-print("How's it hanging "+name+"! You're "+age+" years old, cool story!")
