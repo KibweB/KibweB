34b6ffc5ba4b784bbfc774d028cbb64f9a029150
Kibwe Brandon <47197142+KibweB@users.noreply.github.com>
2023-09-11 12:33:59 -0400

    Add files via upload

---
+++ Simple, beginner code/user input.py
@@ -0,0 +1,8 @@
+print("Welcome to the greeter program")
+name = input("Enter your name: ")
+print("Greetings " + name)
+#printing the sum of user input
+print("\ncalculator program"); 
+first_number = input("\nfirst number: ")
+second_number = input("second number: ")
+print(int(first_number) + int(second_number))
